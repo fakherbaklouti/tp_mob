f6f76f427db98846c939ceab918cb7306eb75742
fakherbaklouti <148551139+fakherbaklouti@users.noreply.github.com>
2023-11-24 15:31:40 +0100

    Add files via upload

---
+++ tp2-mob/fakher_baklouti_lsi3_tp2/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "fakher_baklouti_lsi3_tp2"
+include(":app")
